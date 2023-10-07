8c3b699c0dd29c9a8f657982dc1607579cfba1d2
akratas <cmartinez110@gmail.com>
2023-10-07 16:19:28 -0400

    snapping changes supporting new col names

--- src/nba/analyzer/averages.py
+++ src/nba/analyzer/averages.py
@@ -8,50 +8,61 @@ from caritas.core.depot.abc.api import SQLBasedExternalDepot
 
 from nba.collector.depot import init_db
 
-insert_query: str = 'INSERT INTO NBA.running_player_averages AS avgs ({headers}) ' \
-                    'VALUES (' \
-                    '%(game_date)s' \
-                    ', %(player)s' \
-                    ', %(minutes_played)s' \
-                    ', %(field_goals)s' \
-                    ', %(three_points)s' \
-                    ', %(free_throws)s' \
-                    ', %(offensive_rebounds)s' \
-                    ', %(defensive_rebounds)s' \
-                    ', %(total_rebounds)s' \
-                    ', %(assists)s' \
-                    ', %(steels)s' \
-                    ', %(blocks)s' \
-                    ', %(turn_overs)s' \
-                    ', %(points_scored)s' \
-                    ', %(plus_minus)s' \
-                    ', %(overall_efficiency)s' \
-                    ', %(period_length)s' \
-                    ')' \
-                    'ON CONFLICT (game_date, player, period_length)' \
-                    'DO UPDATE SET' \
-                    '  minutes_played = %(minutes_played)s' \
-                    ', field_goals = %(field_goals)s' \
-                    ', three_points = %(three_points)s' \
-                    ', free_throws = %(free_throws)s' \
-                    ', offensive_rebounds = %(offensive_rebounds)s' \
-                    ', defensive_rebounds = %(defensive_rebounds)s' \
-                    ', total_rebounds = %(total_rebounds)s' \
-                    ', assists = %(assists)s' \
-                    ', steels = %(steels)s' \
-                    ', blocks = %(blocks)s' \
-                    ', turn_overs = %(turn_overs)s' \
-                    ', points_scored = %(points_scored)s' \
-                    ', plus_minus = %(plus_minus)s' \
-                    ', overall_efficiency = %(overall_efficiency)s' \
-                    'WHERE avgs.game_date = %(game_date)s AND avgs.player = %(player)s ' \
-                    'AND avgs.period_length = %(period_length)s'
+_avg_table_name = 'running_player_averages_'
+_insert_query: str = 'INSERT INTO NBA.running_player_averages_{time} AS avgs ({headers}) ' \
+                     'VALUES (' \
+                     '%(game_date)s' \
+                     ', %(player)s' \
+                     ', %(minutes_played)s' \
+                     ', %(field_goals)s' \
+                     ', %(three_points)s' \
+                     ', %(free_throws)s' \
+                     ', %(offensive_rebounds)s' \
+                     ', %(defensive_rebounds)s' \
+                     ', %(total_rebounds)s' \
+                     ', %(assists)s' \
+                     ', %(steels)s' \
+                     ', %(blocks)s' \
+                     ', %(turn_overs)s' \
+                     ', %(points_scored)s' \
+                     ', %(plus_minus)s' \
+                     ', %(overall_efficiency)s' \
+                     ', %(season)s' \
+                     ', %(games_played)s' \
+                     ', %(minutes_per_game)s' \
+                     ', %(center_player_stats)s' \
+                     ', %(guard_player_stats)s' \
+                     ', %(forward_player_stats)s' \
+                     ')' \
+                     'ON CONFLICT (game_date, player)' \
+                     'DO UPDATE SET' \
+                     '  minutes_played = %(minutes_played)s' \
+                     ', field_goals = %(field_goals)s' \
+                     ', three_points = %(three_points)s' \
+                     ', free_throws = %(free_throws)s' \
+                     ', offensive_rebounds = %(offensive_rebounds)s' \
+                     ', defensive_rebounds = %(defensive_rebounds)s' \
+                     ', total_rebounds = %(total_rebounds)s' \
+                     ', assists = %(assists)s' \
+                     ', steels = %(steels)s' \
+                     ', blocks = %(blocks)s' \
+                     ', turn_overs = %(turn_overs)s' \
+                     ', points_scored = %(points_scored)s' \
+                     ', overall_efficiency = %(overall_efficiency)s' \
+                     ', plus_minus = %(plus_minus)s' \
+                     ', season = %(season)s' \
+                     ', games_played = %(games_played)s' \
+                     ', minutes_per_game = %(minutes_per_game)s' \
+                     ', center_player_stats = %(center_player_stats)s' \
+                     ', guard_player_stats = %(guard_player_stats)s' \
+                     ', forward_player_stats = %(forward_player_stats)s' \
+                     'WHERE avgs.game_date = %(game_date)s AND avgs.player = %(player)s '
 
 
 class AveragePeriods(Enum):
     ONE_WEEK = 1 * 7
-    THREE_WEEK = 3 * 7
-    NINE_WEEK = 9 * 7
+    # THREE_WEEK = 3 * 7
+    # NINE_WEEK = 9 * 7
 
 
 class DynamicAveragesCalculator:
@@ -63,16 +74,16 @@ class DynamicAveragesCalculator:
     constant_weight: int = 6
 
     @staticmethod
-    def calculate_averages(stats: Dict[str, Dict[str, any]], rows: List[Dict[str, any]],
+    def calculate_averages(previous_stats: Dict[str, Dict[str, any]], rows: List[Dict[str, any]],
                            columns: List[str], period: AveragePeriods) -> List[Dict[str, any]]:
         multiplier: int = period.value
         averages: List[Dict[str, any]] = []
         for row in rows:
             player: str = row.get('player')
-            stat: Dict[str, any] = stats.get(player)
+            stat: Dict[str, any] = previous_stats.get(player)
             if not stat:
-                stat = {'period_length': multiplier, 'player': player}
-                stats[player] = stat
+                stat = {'player': player}
+                previous_stats[player] = stat
 
             stat['game_date'] = row.get('game_date')
 
@@ -100,7 +111,7 @@ class DynamicAveragesCalculator:
         num: float = current_value if current_value != 0.0 else 0
         denom = num / denom if num != 0.0 and denom != 0.0 else 1
         return prev_average + (current_value - prev_average) / \
-               (DynamicAveragesCalculator.constant_weight * multiplier / denom)
+            (DynamicAveragesCalculator.constant_weight * multiplier / denom)
 
     @staticmethod
     def calc_overall(row: Dict[str, any]) -> float:
@@ -123,24 +134,28 @@ class StatsLoader:
     Takes in parameters and loads stats for calculation purposes.
     """
 
-    game_date_query: str = 'SELECT * FROM NBA.game_stats g WHERE g.game_date BETWEEN \'{from_date}\' AND \'{to_date}\''
-    season_query: str = 'SELECT s.season_start, s.season_end FROM NBA.seasons s WHERE s.season = \'{season}\''
-    headers: str = 'SELECT column_name FROM information_schema.columns WHERE table_schema = \'nba\' ' \
-                   'AND table_name = \'running_player_averages\''
-    numeric_cols_names: str = 'SELECT column_name FROM information_schema.columns WHERE table_schema = \'nba\' ' \
-                              'AND table_name = \'running_player_averages\' AND data_type = \'double precision\';'
-    averages_player: str = 'SELECT player, minutes_played, field_goals, three_points, free_throws, offensive_rebounds' \
-                           ', defensive_rebounds, total_rebounds, assists, steels, blocks, turn_overs, points_scored' \
-                           ', overall_efficiency, period_length FROM NBA.running_player_averages a ' \
-                           'WHERE a.period_length={period} AND a.game_date=(SELECT MAX(game_date) ' \
-                           'FROM NBA.running_player_averages WHERE period_length={second_period})'
+    _game_date_query: str = 'SELECT * FROM NBA.game_stats g WHERE g.game_date BETWEEN \'{from_date}\' AND \'{to_date}\''
+    _season_query: str = 'SELECT s.season_start, s.season_end FROM NBA.seasons s WHERE s.season = \'{season}\''
+    _headers: str = 'SELECT column_name FROM information_schema.columns WHERE table_schema = \'nba\' ' \
+                    'AND table_name = \'{avg_tbl_name}\''
+    _numeric_cols_names: str = 'SELECT column_name FROM information_schema.columns WHERE table_schema = \'nba\' ' \
+                               'AND table_name = \'{avg_tbl_name}\' AND data_type = \'double precision\';'
+    _averages_player: str = 'SELECT player, minutes_played, field_goals, three_points, free_throws, offensive_rebounds'\
+                            ', defensive_rebounds, total_rebounds, assists, steels, blocks, turn_overs, points_scored' \
+                            ', overall_efficiency, period_length FROM NBA.{avg_tbl_name} a ' \
+                            'WHERE a.period_length={period} AND a.game_date=(SELECT MAX(game_date) ' \
+                            'FROM NBA.{avg_tbl_name} WHERE period_length={second_period})'
+    _gams_played: str = 'SELECT player, season, COUNT(season) ' \
+                        'FROM nba.game_stats ' \
+                        'WHERE game_date BETWEEN \'{from}\' AND \'{to}\' AND season=\'{season}\' ' \
+                        'GROUP BY player, season ORDER BY season DESC, player;'
 
     def __init__(self):
         self.depot: SQLBasedExternalDepot = init_db()
 
     def load_by_season(self, season: str) -> List[Dict[str, any]]:
         print(f'Loading data by season {season}.')
-        result: List[Dict[str, any]] = self.depot.do_query_many_dict(StatsLoader.season_query.format(season=season))
+        result: List[Dict[str, any]] = self.depot.do_query_many_dict(StatsLoader._season_query.format(season=season))
         if len(result) == 0:
             raise ValueError(f'Season {season} does not exist.')
 
@@ -151,10 +166,10 @@ class StatsLoader:
 
     def load_by_dates(self, from_date: date, to_date: date) -> List[Dict[str, any]]:
         print(f'Loading data by dates from {from_date} to {to_date}.')
-        return self.depot.do_query_many_dict(StatsLoader.game_date_query.format(from_date=from_date, to_date=to_date))
+        return self.depot.do_query_many_dict(StatsLoader._game_date_query.format(from_date=from_date, to_date=to_date))
 
     def get_averages_headers(self) -> str:
-        rows: List[Dict[str, any]] = self.depot.do_query_many_dict(StatsLoader.headers)
+        rows: List[Dict[str, any]] = self.depot.do_query_many_dict(StatsLoader._headers)
         size: int = len(rows) - 1
         pointer: int = 0
         headers: str = ''
@@ -164,7 +179,7 @@ class StatsLoader:
         return headers
 
     def get_averages_column_names(self) -> List[str]:
-        rows: List[Dict[str, any]] = self.depot.do_query_many_dict(StatsLoader.numeric_cols_names)
+        rows: List[Dict[str, any]] = self.depot.do_query_many_dict(StatsLoader._numeric_cols_names)
         columns: List[str] = []
         for row in rows:
             columns.append(row.get('column_name'))
@@ -172,8 +187,10 @@ class StatsLoader:
 
     def load_previous_player_stats(self, period: AveragePeriods) -> Dict[str, Dict[str, any]]:
         rows: List[Dict[str, any]] = \
-            self.depot.do_query_many_dict(StatsLoader.averages_player.format(period=period.value,
-                                                                             second_period=period.value))
+            self.depot.do_query_many_dict(
+                StatsLoader._averages_player.format(avg_tbl_name=f'{_avg_table_name}{period.name}',
+                                                    period=period.value,
+                                                    second_period=period.value))
         stats: Dict[str, Dict[str, any]] = {}
         for row in rows:
             player: str = row.get('player')
@@ -205,7 +222,8 @@ class AnalyticsController:
         for period in AveragePeriods:
             stats: Dict[str, Dict[str, any]] = self.loader.load_previous_player_stats(period)
             result: List[Dict[str, any]] = DynamicAveragesCalculator.calculate_averages(stats, rows, columns, period)
-            self.depot.do_batch_query_single_dict(insert_query.format(headers=headers), False, result)
+            self.depot.do_batch_query_single_dict(_insert_query.format(time=period.name, headers=headers), False,
+                                                  result)
 
 
 def main(args):
